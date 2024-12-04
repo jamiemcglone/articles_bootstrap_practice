5a4af7481307a4675b52318114c830976c967a6e
Jamie McGlone <jamie.mcglone@outlook.com>
2024-12-04 11:17:58 +0000

    Added more styling, got create and update actions working.

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,39 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to articles_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find_by(id: params[:id])
+  end
+
+  def update
+    @article = Article.find_by(id: params[:id])
+    if @article.update(article_params)
+      redirect_to articles_path
+      flash[:success] = "Article updated"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+end
